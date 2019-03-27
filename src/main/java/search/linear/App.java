2aea3cdca15c66bb6faa4da641ff962c64958615
refrain <nao_h13@hotmail.com>
2019-03-28 01:03:20 +0900

    リニアサーチの実装

---
+++ src/main/java/search/linear/App.java
@@ -0,0 +1,56 @@
+package search.linear;
+
+import java.util.Scanner;
+
+public class App
+{
+	public static void main( String[] args )
+	{
+		// 標準入力を使う準備
+		Scanner scanner = new Scanner(System.in);
+		
+		LinearSearch linearSearch = LinearSearch.getInstance();
+		
+		int result = LinearSearch.C_NOT_FOUND;
+		int[] searchData = { 1, 2, 3, 4, 5 };
+
+		int searchVal = -999;
+		String strDelimiter = "";
+		
+		// メッセージ表示
+		System.out.println("App Start!");
+		System.out.print("Array Data is [");
+
+		strDelimiter = "";
+		
+		for( int val : searchData )
+		{
+			System.out.print( strDelimiter + val );
+			
+			strDelimiter = ", ";
+		}
+		System.out.println("]");
+		
+		// キー入力（数値として）
+		System.out.println("What is the search value of array data？");
+		searchVal = scanner.nextInt();
+		
+		// 探索
+		result = linearSearch.execute(
+										searchData,
+										searchVal
+										);
+		
+		// 探索結果NG
+		if(		LinearSearch.C_NOT_FOUND == result
+			)
+		{
+			System.out.println("Data Not Found.");
+		}
+		// 探索OK
+		else
+		{
+			System.out.println("Data Found. Array index is " + (result + 1) );	
+		}
+	}
+}
---
+++ src/main/java/search/linear/LinearSearch.java
@@ -0,0 +1,53 @@
+package search.linear;
+
+/*
+平均計算量： O(n)
+
+リストの先頭から要素を取り出す
+取り出した要素の値と探したい要素の値を比較する
+・一致すれば探索完了
+・一致しなければ次の要素を取り出す
+*/
+
+public class LinearSearch
+{
+	// Singleton用
+	private static LinearSearch linearSearch = new LinearSearch(); 
+	
+	// 見つからなかった場合の戻り値
+	public static int C_NOT_FOUND = -1;
+	
+	// 外部からインスタンス化できないようにコンストラクタをprivate化
+	private LinearSearch()
+	{	
+	}
+	
+	// インスタンスを返す
+	public static LinearSearch getInstance()
+	{
+		return linearSearch;
+	}
+	
+	// 探索実行
+	public int execute(
+						int[] argData,
+						int argVal
+						)
+	{
+		if(		argData != null
+			)
+		{
+			// ループで探索し、一致したらインデックスを返す
+			for( int i = 0; i < argData.length; i++ )
+			{
+				if(		argData[ i ] == argVal
+					)
+				{
+					return i;
+				}
+			}
+		}
+		
+		return C_NOT_FOUND;
+	}
+}
