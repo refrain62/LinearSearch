69742553d2d85cd74a9089115abcf80c162d6f49
refrain <nao_h13@hotmail.com>
2019-03-29 00:13:25 +0900

    実行クラスのリファクタリング

--- src/main/java/search/linear/App.java
+++ src/main/java/search/linear/App.java
@@ -6,34 +6,23 @@ public class App
 {
 	public static void main( String[] args )
 	{
-		// 標準入力を使う準備
-		Scanner scanner = new Scanner(System.in);
-		
 		LinearSearch linearSearch = LinearSearch.getInstance();
-		
 		int result = LinearSearch.C_NOT_FOUND;
+		
+		// 探索データの定義
 		int[] searchData = { 1, 2, 3, 4, 5 };
 
 		int searchVal = -999;
-		String strDelimiter = "";
 		
-		// メッセージ表示
 		System.out.println("Linear Search App Start!");
-		System.out.print("Array Data is [");
-
-		strDelimiter = "";
 		
-		for( int val : searchData )
-		{
-			System.out.print( strDelimiter + val );
-			
-			strDelimiter = ", ";
-		}
-		System.out.println("]");
+		// 検索データの出力
+		outputSearchData(
+						searchData
+						);
 		
 		// キー入力（数値として）
-		System.out.println("What is the search value of array data？");
-		searchVal = scanner.nextInt();
+		searchVal = inputKeyValue();
 		
 		// 探索
 		result = linearSearch.execute(
@@ -41,8 +30,68 @@ public class App
 										searchVal
 										);
 		
+		// 結果の出力
+		outputResult(
+							result,
+							LinearSearch.C_NOT_FOUND
+							);
+	}
+
+	/**
+	 * 検索データの出力
+	 * @param argSearchData
+	 * @return
+	 */
+	private static void outputSearchData(
+										int[] argSearchData
+										)
+	{
+		String strDelimiter = "";
+		
+		System.out.print("Array Data is [");
+
+		strDelimiter = "";
+		
+		for( int val : argSearchData )
+		{
+			System.out.print( strDelimiter + val );
+			
+			strDelimiter = ", ";
+		}
+		
+		System.out.println("]");
+	}
+	
+	/**
+	 *  キー入力（数値として）
+	 * @return
+	 */
+	private static int inputKeyValue()
+	{
+		int searchVal = -999;
+		
+		// 標準入力を使う準備
+		Scanner scanner = new Scanner(System.in);
+
+		System.out.println("What is the search value of array data？");
+		
+		searchVal = scanner.nextInt();
+		
+		return searchVal;
+	}
+	
+	/**
+	 *  結果の出力
+	 * @param argResult
+	 * @param argNotFoundValue
+	 */
+	private static void outputResult(
+								int argResult,
+								int argNotFoundValue
+							)
+	{
 		// 探索結果NG
-		if(		LinearSearch.C_NOT_FOUND == result
+		if(		argNotFoundValue == argResult
 			)
 		{
 			System.out.println("Data Not Found.");
@@ -50,7 +99,7 @@ public class App
 		// 探索OK
 		else
 		{
-			System.out.println("Data Found. Array index is " + (result + 1) );	
+			System.out.println("Data Found. Array index is " + (argResult + 1) );	
 		}
 	}
 }
