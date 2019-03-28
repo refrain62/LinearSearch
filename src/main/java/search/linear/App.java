0196beb1551ee6b8b109be948a51cb9e42f5137f
refrain <nao_h13@hotmail.com>
2019-03-29 00:33:51 +0900

    タブ文字を空白に変更

--- src/main/java/search/linear/App.java
+++ src/main/java/search/linear/App.java
@@ -6,115 +6,115 @@ import java.io.InputStreamReader;
 
 public class App
 {
-	public static void main( String[] args )
-	{
-		LinearSearch linearSearch = LinearSearch.getInstance();
-		
-		int result = LinearSearch.C_NOT_FOUND;
-		int searchVal = -999;
-		
-		// 探索データの定義
-		int[] searchData = { 1, 2, 3, 4, 5 };
+  public static void main( String[] args )
+  {
+    LinearSearch linearSearch = LinearSearch.getInstance();
+    
+    int result = LinearSearch.C_NOT_FOUND;
+    int searchVal = -999;
+    
+    // 探索データの定義
+    int[] searchData = { 1, 2, 3, 4, 5 };
 
-		System.out.println("Linear Search App Start!");
-		
-		// 検索データの出力
-		outputSearchData(
-						searchData
-						);
-		
-		// キー入力（数値として）
-		searchVal = inputKeyValue();
-		
-		// 探索
-		result = linearSearch.execute(
-										searchData,
-										searchVal
-										);
-		
-		// 結果の出力
-		outputResult(
-							result,
-							LinearSearch.C_NOT_FOUND
-							);
-	}
+    System.out.println("Linear Search App Start!");
+    
+    // 検索データの出力
+    outputSearchData(
+            searchData
+            );
+    
+    // キー入力（数値として）
+    searchVal = inputKeyValue();
+    
+    // 探索
+    result = linearSearch.execute(
+                    searchData,
+                    searchVal
+                    );
+    
+    // 結果の出力
+    outputResult(
+              result,
+              LinearSearch.C_NOT_FOUND
+              );
+  }
 
-	/**
-	 * 検索データの出力
-	 * @param argSearchData
-	 * @return
-	 */
-	private static void outputSearchData(
-										int[] argSearchData
-										)
-	{
-		String strDelimiter = "";
-		
-		System.out.print("Array Data is [");
+  /**
+   * 検索データの出力
+   * @param argSearchData
+   * @return
+   */
+  private static void outputSearchData(
+                    int[] argSearchData
+                    )
+  {
+    String strDelimiter = "";
+    
+    System.out.print("Array Data is [");
 
-		strDelimiter = "";
-		
-		for( int val : argSearchData )
-		{
-			System.out.print( strDelimiter + val );
-			
-			strDelimiter = ", ";
-		}
-		
-		System.out.println("]");
-	}
-	
-	/**
-	 *  キー入力（数値として）
-	 * @return
-	 */
-	private static int inputKeyValue()
-	{
-	    String input = null;
-	    int number = 0;
+    strDelimiter = "";
+    
+    for( int val : argSearchData )
+    {
+      System.out.print( strDelimiter + val );
+      
+      strDelimiter = ", ";
+    }
+    
+    System.out.println("]");
+  }
+  
+  /**
+   *  キー入力（数値として）
+   * @return
+   */
+  private static int inputKeyValue()
+  {
+      String input = null;
+      int number = 0;
 
-		System.out.println("What is the search value of array data？");
-		
-	    try
-	    {
-	        BufferedReader bufferedReader = new BufferedReader( new InputStreamReader( System.in ) );
-	        
-	        input = bufferedReader.readLine();
-	        
-	        number = Integer.parseInt(input);
-	    }
-	    catch( NumberFormatException ex )
-	    {
-	       System.out.println("Not a number !");
-	    }
-	    catch( IOException e )
-	    {
-	        e.printStackTrace();
-	    }
-	    
-	    return number;
-	}
-	
-	/**
-	 *  結果の出力
-	 * @param argResult
-	 * @param argNotFoundValue
-	 */
-	private static void outputResult(
-								int argResult,
-								int argNotFoundValue
-							)
-	{
-		// 探索結果NG
-		if(		argNotFoundValue == argResult
-			)
-		{
-			System.out.println("Data Not Found.");
-		}
-		// 探索OK
-		else
-		{
-			System.out.println("Data Found. Array index is " + (argResult + 1) );	
-		}
-	}
+    System.out.println("What is the search value of array data？");
+    
+      try
+      {
+          BufferedReader bufferedReader = new BufferedReader( new InputStreamReader( System.in ) );
+          
+          input = bufferedReader.readLine();
+          
+          number = Integer.parseInt(input);
+      }
+      catch( NumberFormatException ex )
+      {
+         System.out.println("Not a number !");
+      }
+      catch( IOException e )
+      {
+          e.printStackTrace();
+      }
+      
+      return number;
+  }
+  
+  /**
+   *  結果の出力
+   * @param argResult
+   * @param argNotFoundValue
+   */
+  private static void outputResult(
+                int argResult,
+                int argNotFoundValue
+              )
+  {
+    // 探索結果NG
+    if(   argNotFoundValue == argResult
+      )
+    {
+      System.out.println("Data Not Found.");
+    }
+    // 探索OK
+    else
+    {
+      System.out.println("Data Found. Array index is " + (argResult + 1) ); 
+    }
+  }
 }
--- src/main/java/search/linear/LinearSearch.java
+++ src/main/java/search/linear/LinearSearch.java
@@ -11,46 +11,46 @@ package search.linear;
 
 public class LinearSearch
 {
-	// Singleton用
-	private static LinearSearch linearSearch = new LinearSearch(); 
-	
-	// 見つからなかった場合の戻り値
-	public static int C_NOT_FOUND = -1;
-	
-	// 外部からインスタンス化できないようにコンストラクタをprivate化
-	private LinearSearch()
-	{	
-	}
-	
-	// インスタンスを返す
-	public static LinearSearch getInstance()
-	{
-		return linearSearch;
-	}
-	
-	// 探索実行
-	public int execute(
-						int[] argData,
-						int argVal
-						)
-	{
-		if(		argData != null
-			)
-		{
-			// ループで探索し、一致したらインデックスを返す
-			for( int i = 0; i < argData.length; i++ )
-			{
-				// 情報をログに出力
-				System.out.println( "index : " + i + " val: " + argData[ i ] );
-				
-				if(		argData[ i ] == argVal
-					)
-				{
-					return i;
-				}
-			}
-		}
-		
-		return C_NOT_FOUND;
-	}
+  // Singleton用
+  private static LinearSearch linearSearch = new LinearSearch(); 
+  
+  // 見つからなかった場合の戻り値
+  public static int C_NOT_FOUND = -1;
+  
+  // 外部からインスタンス化できないようにコンストラクタをprivate化
+  private LinearSearch()
+  { 
+  }
+  
+  // インスタンスを返す
+  public static LinearSearch getInstance()
+  {
+    return linearSearch;
+  }
+  
+  // 探索実行
+  public int execute(
+            int[] argData,
+            int argVal
+            )
+  {
+    if(   argData != null
+      )
+    {
+      // ループで探索し、一致したらインデックスを返す
+      for( int i = 0; i < argData.length; i++ )
+      {
+        // 情報をログに出力
+        System.out.println( "index : " + i + " val: " + argData[ i ] );
+        
+        if(   argData[ i ] == argVal
+          )
+        {
+          return i;
+        }
+      }
+    }
+    
+    return C_NOT_FOUND;
+  }
 }
